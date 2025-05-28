adf6c24004ae8c602eb61e60e6ba875ec9f65d20
Rogdham <contact@rogdham.net>
2025-05-28 20:47:48 +0200

    feat: update code from upstream 3.14.0b2

--- src/c/compat/backports_zstd_edits.h
+++ src/c/compat/backports_zstd_edits.h
@@ -45,4 +45,54 @@ BACKPORTSZSTD__PyArg_UnpackKeywords(
 #define BACKPORTSZSTD__PyNumber_Index(o) \
     _PyNumber_Index(o)
 
+#if PY_VERSION_HEX < 0x030D0000 // Python 3.12 and below
+#define BACKPORTSZSTD_PyMutex PyThread_type_lock
+#define BACKPORTSZSTD_PyMutex_IsNull(l) (l == NULL)
+#define BACKPORTSZSTD_PyMutex_allocate PyThread_allocate_lock
+static inline void BACKPORTSZSTD_PyMutex_Lock(PyThread_type_lock *mp)
+{
+    Py_BEGIN_ALLOW_THREADS
+    PyThread_acquire_lock(*mp, WAIT_LOCK);
+    Py_END_ALLOW_THREADS
+}
+static inline void BACKPORTSZSTD_PyMutex_Unlock(PyThread_type_lock *mp)
+{
+    PyThread_release_lock(*mp);
+}
+static inline void BACKPORTSZSTD_PyMutex_free(PyThread_type_lock mp)
+{
+    if (mp)
+    {
+        PyThread_free_lock(mp);
+    }
+}
+static inline int BACKPORTSZSTD_PyMutex_IsLocked(PyThread_type_lock *mp)
+{
+    // note: this function is only used in asserts
+    PyLockStatus status;
+    Py_BEGIN_ALLOW_THREADS
+    status = PyThread_acquire_lock_timed(*mp, 0, 0);
+    Py_END_ALLOW_THREADS
+    if (status == PY_LOCK_ACQUIRED)
+    {
+        PyThread_release_lock(*mp);
+        return 0;
+    }
+    return 1;
+}
+#else
+#define BACKPORTSZSTD_PyMutex PyMutex
+#define BACKPORTSZSTD_PyMutex_IsNull(l) (0)
+#define BACKPORTSZSTD_PyMutex_allocate() ((PyMutex){0})
+#define BACKPORTSZSTD_PyMutex_Lock PyMutex_Lock
+#define BACKPORTSZSTD_PyMutex_Unlock PyMutex_Unlock
+#define BACKPORTSZSTD_PyMutex_free(l)
+static inline int BACKPORTSZSTD_PyMutex_IsLocked(PyMutex *lp)
+{
+    // note: this function is only used in asserts
+    // PyMutex_IsLocked is not exposed publicly https://github.com/python/cpython/issues/134009
+    Py_FatalError("Not implemented");
+}
+#endif
+
 #endif /* !BACKPORTS_ZSTD_EDITS_H */
